c364f06a19dcab53f3c9382756829d0ca1e5d62b
axazaff <axazaff@gmail.com>
2019-11-29 23:21:33 +0000

    adding initial test framework

---
+++ src/main/java/co/uk/moveright/App.java
@@ -0,0 +1,13 @@
+package co.uk.moveright;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
